b92ceda198c0a5243fa0e5615f8d367d18302a3d
acesmndr <acesmndr@gmail.com>
2014-01-09 14:49:57 +0545

    selectgroup switch updated and support for php addition added and proper ifelse syntax applied

--- lsr.js
+++ lsr.js
@@ -18,6 +18,15 @@ var ampm1,ampm2;
 var grp123 = document.getElementById("groupshow");
 
 window.onload = function() {
+	//php compatibility  update with the values of php variables in if statement
+	if(typeof b !== "undefined"){
+		alert("value of array changed");
+		for(i=0;i<7;i++){
+			for(j=0;j<4;j++){
+				a[i][j]=b[i][j];
+			}
+	}				
+	}	
 ace(1);//to show the routine of group 1 on start
 //document.getElementById('dayname').innerHTML=darray[day];//to display today's day of week
 }
@@ -34,87 +43,75 @@ day_flag=1;
 day_select=number;
 clearInterval(intid);//stop the currently running countdown function
 var flag=0;
-for(var i=0;i<6;i=i+2)//interval 0-6 for 0-3 routine schedules per day with start and end times
-{
-grp_no=number-day-1;
-if(grp_no<0)
-{
-grp_no=7+grp_no;//less than 0 so negative hence added previously was a bug
-}
+for(var i=0;i<6;i=i+2){//interval 0-6 for 0-3 routine schedules per day with start and end times
+	grp_no=number-day-1;
+	if(grp_no<0){
+		grp_no=7+grp_no;//less than 0 so negative hence added previously was a bug
+	}
 var ini=a[grp_no][i];//cutoff time value
-if(ini>12)
-{
-ampm1=ini-12;//24 to 12 hour format conversion
-ampm1+=" pm";//addition of am or pm
+if(ini>12){
+	ampm1=ini-12;//24 to 12 hour format conversion
+	ampm1+=" pm";//addition of am or pm
 }
 else
-ampm1=ini+" am";
+{
+	ampm1=ini+" am";
+}
 var end=a[grp_no][i+1];//Electricity return back tiem value
 if(end>12)
 {
-ampm2=end-12;
-ampm2+=" pm";
+	ampm2=end-12;
+	ampm2+=" pm";
 }
 else
-ampm2=end+" am";
-
+{
+	ampm2=end+" am";
+}
 var j=i/2;
 //if there is a schedule ie there is a cutoff time then display time else display nothing in the html body
-if(ini!=undefined) 
+if(ini!=undefined){ 
 	document.getElementById('time'+[j]).innerHTML=ampm1+" - "+ampm2;
-else
+}else{
 	document.getElementById('time'+[j]).innerHTML=" ";
+}
 //In cutoff conditiion and flag is unset set flag to cutoff state
 //else if electricity is on and is before cutoff time and flag is unset set flag to live state
-if((our>=ini)&&(our<end)&&(flag==0))
-{
+if((our>=ini)&&(our<end)&&(flag==0)){
 	rem=end-our-1;
 	flag=2;
-}
-else
-{
-	if((our<ini)&&(flag==0))
-	{
+}else{
+	if((our<ini)&&(flag==0)){
 		rem=ini-our-1;
 		flag=1;
 		}
 	}
 }
 //if flag is unset then the time remaining is compared with the first cutoff time of next day and the flag is again set to represent live state
-if(flag==0)
-	{
-		if(grp_no==0)
+if(flag==0){
+		if(grp_no==0){
 			rem=24-our-1+a[6][0];
-		else
+		}else{
 			rem=24-our-1+a[grp_no-1][0];
+		}
 		flag=1;
 }
 //function for the countdown from current time to zerro
 var tick_tock=function()
 {
-if(sec==0)
-{
-	if(mint!=0)
-	{
+if(sec==0){
+	if(mint!=0){
 		mint--;
-	}
-	else
-	{
+	}else{
 		mint=59;
-		if(rem!=0)
-		{
+		if(rem!=0){
 			rem--;
-		}
-		else
-		{
+		}else{
 			ace(number);
 		}
 	}
 sec=59;
-}
-else
-{
-sec--;
+}else{
+	sec--;
 }
 //set the type to cutoff for all cases
 var type="for Electricity";
@@ -122,11 +119,10 @@ var type="for Electricity";
 document.getElementById("line1").className="hr1";
 document.getElementById("line2").className="hr1";
 //if flag is set to live  state then set the colour of line to green
-if(flag==1)
-{
-document.getElementById("line1").className="hr2";
-document.getElementById("line2").className="hr2";
-type="for Cutoff";
+if(flag==1){
+	document.getElementById("line1").className="hr2";
+	document.getElementById("line2").className="hr2";
+	type="for Cutoff";
 }
 //display countdown
 document.getElementById("timer").innerHTML=" "+rem+" hour "+mint+" minutes and "+sec+" seconds "+type;
@@ -138,10 +134,11 @@ intid=setInterval(function(){tick_tock();},1000);//recursive call for the realti
 function prv()
 {
 	//checking and decreasing the value of the today's day
-	if(day_chg==0)
+	if(day_chg==0){
 		day_chg=6;
-	else
+	}else{
 		day_chg--;
+	}
 	//display today's day	
 	document.getElementById('dayname').innerHTML=darray[day_chg];
 	day_flag=0;//reset the day flag to let the function ace(parameter) know it was called by nxt function not grouplist
@@ -153,10 +150,11 @@ function prv()
 }
 function nxt()
 {
-	if(day_chg==6)
-		day_chg=0;
-	else
+	if(day_chg==6){
+			day_chg=0;
+	}else{
 		day_chg++;
+	}
 	document.getElementById('dayname').innerHTML=darray[day_chg];
 	day_flag=0;
 	dselect_condn=day_select;
@@ -166,18 +164,13 @@ function nxt()
 }
 //function to unhide the group list after the group switch has been pressed
 function groupUnhide() {
+	if(grp123.style.visibility=="hidden"){
 	grp123.style.visibility="visible";
+	}else{
+	grp123.style.visibility="hidden";
+	}
 	document.getElementById("grp_switch").style.visibility="visible";
-	/*var display = el.style.display;
-
-        if (display == "none") {
-            el.style.display = "block";
-	document.getElementById("grp_switch").style.display="none";
-        } else {
-	
-            el.style.display = "none";
-        }*/
-    }
+}
 //function to switch the display of about us's content
  function contact(){
 	if(document.getElementById("background").style.display=="block")
